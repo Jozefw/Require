28c3f94029fc16a1663a0931be956c231578b456
Jozef <jozefw@sbcglobal.net>
2015-04-21 12:40:48 -0700

    module without config file

--- requireConfig.js
+++ requireConfig.js
@@ -1,7 +1,13 @@
-require.config(
-{
-	paths: {
-		'jquery': ['http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min',
-								'jquery.js']
-	}
-});
+require(['jquery'],function($){
+	$('.output').html("So long, and thanks for all the fish!");
+})
+
+// SO this could be the config file that conigures all your paths.  
+// Require starts looking for files at the root path set in the data-main
+// require.config(
+// {
+// 	paths: {
+// 		'jquery': ['http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min',
+// 								'jquery.js']
+// 	}
+// });
